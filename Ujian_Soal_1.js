d68bf4f1c22d8b47cfa28901a89975399e7dda94
Adelbertus27 <44288503+Adelbertus27@users.noreply.github.com>
2018-11-06 10:33:16 +0700

    Add files via upload

---
+++ Ujian_Soal_1.js
@@ -0,0 +1,11 @@
+function fpb_kpk(x,y) {
+    for(i=1;i<=x;i++){
+        fpb = Math.floor(i) 
+        console.log('FPB ' + x + ' & ' + y +' = '+Math.floor(fpb))
+        for(i=1;i<=y;i++){
+            }  
+    } kpk = Math.max(x) * Math.max(y)
+    console.log('KPK ' + x + ' & ' + y +' = '+kpk)
+}
+
+fpb_kpk(2,3)
